a2bc5b7106dfd055b22d34ae785ddb64fb142718
abhishek <abhishekgharami1998@gmail.com>
2024-12-02 15:53:39 +0530

    Adding the data hierarchy for the model to admin .

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -12,3 +12,5 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title','slug']
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status' , 'publish']
