49f1cee3122fa94440cfd7af57d7b94d5e921cff
abhishek <abhishekgharami1998@gmail.com>
2024-12-05 16:03:47 +0530

    Adding the migrations for the repo .

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,7 +17,10 @@ class Post(models.Model):
         DRAFT = 'DF' ,'Draft'
         PUBLISHED = 'PB' , 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,unique=True)
+    slug = models.SlugField(
+                            max_length=250,
+                            unique_for_date='publish'
+                            )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
