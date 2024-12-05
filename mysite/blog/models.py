338a09fca92d30edb2588de1e21e4b638ce2d79f
abhishek <abhishekgharami1998@gmail.com>
2024-12-05 16:16:53 +0530

    for the seo friendly for the user .

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -49,7 +49,10 @@ class Post(models.Model):
     # cannonical url for the site
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       args=[self.id])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
     
     
     def __str__(self):
