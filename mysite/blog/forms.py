9f14c2fc6197d31420d94d699ddecda111318231
abhishek <abhishekgharami1998@gmail.com>
2024-12-06 19:37:34 +0530

    Adding the form for the comment.

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,5 +1,6 @@
 # build the form to share the content to the user email
 from django import forms
+from .models import Comment
 
 # class for the form template
 class EmailPostForm(forms.Form):
@@ -11,4 +12,10 @@ class EmailPostForm(forms.Form):
         widget=forms.Textarea
     )
     
+    
+# creating the form for the comment for the user
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name','email','body']
 
