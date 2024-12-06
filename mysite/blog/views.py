c76668ac2e330ac2a556d07ae2a1c98bb6133266
abhishek <abhishekgharami1998@gmail.com>
2024-12-06 10:54:42 +0530

    Adding the try and catch block for the user to load wrong page number .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render ,get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator , EmptyPage ,PageNotAnInteger
 from .models import Post
 from django.http import Http404
 
@@ -10,7 +10,14 @@ def post_list(request):
     post_list = Post.objects.all()
     paginator = Paginator(post_list,3)
     page_number = request.GET.get('page',1)
-    posts = paginator.page(page_number)
+    # using try and catch block to prevention is empty page or if user try tot enter the wrong page number
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # then goes to the page 1
+        posts = paginator.page(1)
     #try to render it
     return render(request,
                   'blog/post/list.html',{
