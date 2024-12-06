d55c7f431e518ea200a3233534f2359daea03073
abhishek <abhishekgharami1998@gmail.com>
2024-12-06 11:03:15 +0530

    Added the class based view for the list .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,27 +2,36 @@ from django.shortcuts import render ,get_object_or_404
 from django.core.paginator import Paginator , EmptyPage ,PageNotAnInteger
 from .models import Post
 from django.http import Http404
+from django.views.generic import ListView
 
 # Create your views here.
 
 # first view for create all the get request for the posts in it
-def post_list(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list,3)
-    page_number = request.GET.get('page',1)
-    # using try and catch block to prevention is empty page or if user try tot enter the wrong page number
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        # then goes to the page 1
-        posts = paginator.page(1)
-    #try to render it
-    return render(request,
-                  'blog/post/list.html',{
-                      'posts':posts
-                  })
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     paginator = Paginator(post_list,3)
+#     page_number = request.GET.get('page',1)
+#     #using try and catch block to prevention is empty page or if user try tot enter the wrong page number
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         # then goes to the page 1
+#         posts = paginator.page(1)
+#     #try to render it
+#     return render(request,
+#                   'blog/post/list.html',{
+#                       'posts':posts
+#                   })
+
+
+# now going to use the class based views for the user
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
     
     
     
