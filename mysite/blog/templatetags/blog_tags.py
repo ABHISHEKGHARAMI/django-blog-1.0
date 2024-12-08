f14754538d95b3aac4e1c1e554ff03dfba8aa7f3
abhishek <abhishekgharami1998@gmail.com>
2024-12-08 17:29:54 +0530

    Added the latest post by tag.

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -7,3 +7,13 @@ register = template.Library()
 @register.simple_tag
 def total_tags():
     return Post.published.count()
+
+
+@register.inclusion_tag("blog/post/latest.html")
+def latest_tag(count=5):
+    latest_posts = Post.published.order_by('-publish')[:5]
+    
+    return {
+        'latest_posts': latest_posts
+        
+    }
