0cbaa3afb6a6e97b6cc617776f0b48470de14594
abhishek <abhishekgharami1998@gmail.com>
2024-12-16 10:42:32 +0530

    Adding the sitemap for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+SITE_ID = 1
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'taggit',
     'blog.apps.BlogConfig',
